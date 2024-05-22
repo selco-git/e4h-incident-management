a330f45a804baf45f7a69373aedb5b74bcf3940a
deepikaarora88 <deepika.arora@pwc.com>
2024-05-22 15:11:07 +0000

    updated content type

--- backend/core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/config/SMSProperties.java
+++ backend/core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/config/SMSProperties.java
@@ -17,7 +17,7 @@ public class SMSProperties {
     @Value("${sms.provider.requestType}")
     public String requestType;
 
-    @Value("${sms.provider.contentType:application/x-www-form-urlencoded}")
+    @Value("${sms.provider.contentType:application/json}")
     public String contentType;
 
     @Value("${sms.mobile.prefix:}")
